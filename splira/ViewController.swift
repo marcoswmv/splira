22c678be10c60b44c83ecb31387e6d0b9a7af48d
Marcos Vicente <marcos.mesquita.vicente@sibs.pt>
2024-09-16 15:55:44 +0100

    Initial Commit

---
+++ splira/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  splira
+//
+//  Created by Marcos Vicente on 16/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
