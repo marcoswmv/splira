353207582e1baad0e879b6d6ef916ad362d7f629
Marcos Vicente <marcoswmvicente@gmail.com>
2024-10-11 19:11:03 +0100

    Initial commit

---
+++ splira/spliraApp.swift
@@ -0,0 +1,17 @@
+//
+//  spliraApp.swift
+//  splira
+//
+//  Created by Marcos Vicente on 11/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct spliraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
